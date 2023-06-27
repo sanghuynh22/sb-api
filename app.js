e14d2fd1d9d6fa6d3ad2b7a542ac640a1c9d4356
sanghuynh22 <93753785+sanghuynh22@users.noreply.github.com>
2023-06-27 19:52:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.json({ limit: "50mb" }));
 // 	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
 // 	next();
 // });
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 // Define API endpoints
 app.use("/api/users", usersRoutes);
 app.use("/api/status", statusRoutes);
